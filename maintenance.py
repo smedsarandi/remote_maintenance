d6474491f34f434f9aa54ded13284b5d38c33425
fontesthiago <thiago.fontes.web@gmail.com>
2024-06-27 08:10:54 -0300

    mudado para POO

--- maintenance.py
+++ maintenance.py
@@ -1,10 +1,14 @@
-#Este módulo deverá ser chamado sempre que iniciar o usuário. Ele será instalado pelo módulo "install"
-import os, zipfile, time, glob, socket, json, requests, shutil, subprocess
-hostname = socket.gethostname()
-directory_executor = 'c:/apps/'
-os.chdir(directory_executor)#definindo o local de trabalho do executável
+import os, zipfile, time, socket, json, requests, shutil, psutil, subprocess, threading
 
-#Classe que gerenciará os apps já baixados e possivelmente em execução que foram filtrados pela Função get_new_app()
+hostname = socket.gethostname()
+
+# Verificação de existência do diretório
+directory_executor = 'c:/apps/'
+if not os.path.exists(directory_executor):
+    os.makedirs(directory_executor)
+os.chdir(directory_executor)
+
+# Classe que gerenciará os apps já baixados e possivelmente em execução que foram filtrados pela Função get_new_app()
 class App_manager:
     def __init__(self, app_name, version, url_download, executable_name):
         self.app_name = app_name
@@ -13,125 +17,131 @@ class App_manager:
         self.executable_name = executable_name
         self.status = {}
 
-    #metodo que apenas baixa o arquivo .zip e o extrai no 'directory_executor'
+    # Método que APENAS BAIXA E EXTRAI o arquivo .zip no 'directory_executor'
     def app_download(self):
+        print(f'\n\nSOLICITADO DOWNLOAD PARA {self.app_name}')
         try:
             response = requests.get(self.url_download, stream=True)
             if response.status_code == 200:
-                print('statuscode 200') 
+                print(f'{self.app_name}: statuscode 200') 
                 with open(f'{self.app_name}.zip', 'wb') as file:
-                    print('baixando o arquivo .zip')
+                    print(f'{self.app_name}: baixando o arquivo .zip')
                     response.raw.decode_content = True
                     shutil.copyfileobj(response.raw, file)
             
                 with zipfile.ZipFile(f'{self.app_name}.zip', 'r') as Zip:
-                    print(f'extraido {self.app_name}.zip')
+                    print(f'{self.app_name}: extraido {self.app_name}.zip')
                     Zip.extractall()
                     time.sleep(5)
-                print('excluindo o .zip baixado')
+                print(f'{self.app_name}: excluindo o .zip baixado')
                 os.remove(f'{self.app_name}.zip')
                 return True
             else:
-                print('statuscode do download não é 200')
+                print(f'{self.app_name}: statuscode do download não é 200')
                 return False
-        except:
+        except requests.exceptions.RequestException as e:
+            print(f'{self.app_name}: Erro ao baixar o arquivo: {e}')
+            return False
+        except zipfile.BadZipFile as e:
+            print(f'{self.app_name}: Erro ao extrair o arquivo zip: {e}')
+            return False
+        except Exception as e:
+            print(f'{self.app_name}: Erro inesperado: {e}')
             return False
 
-    #metodo que inicia o app baixado pelo 'app_download()'
+    # Método que INICIA O APP
     def app_start(self):
+        print(f'\n\nSOLICITADO START PARA {self.app_name}')
         try:
             executable_path = os.path.join(directory_executor, self.executable_name)
             if os.path.exists(executable_path):
-                print(f'Iniciando o app {self.executable_name}')
+                print(f'{self.app_name}: Iniciando o app {self.executable_name}')
                 subprocess.Popen([executable_path], shell=True)
             else:
-                print(f'O executável {self.executable_name} não foi encontrado no diretório {directory_executor}')
+                print(f'{self.app_name}: O executável {self.executable_name} não foi encontrado no diretório {directory_executor}')
         except Exception as e:
-            print(f'Erro ao iniciar o app: {e}')
+            print(f'{self.app_name}: Erro ao iniciar o app: {e}')
 
+    # Método que PARA O APP
     def app_stop(self):
-        pass
+        print(f'\n\nSOLICITADO STOP PARA {self.app_name}')
+        for proc in psutil.process_iter(['pid', 'name']):
+            try:
+                if self.executable_name.lower() in proc.info['name'].lower():
+                    print(f"{self.app_name}: Finalizando processo: {proc.info['name']} (PID: {proc.info['pid']})")
+                    proc.terminate()  # Envia o sinal de término
+                    proc.wait(timeout=5)  # Espera que o processo termine
+                    print(f"{self.app_name}: Processo {proc.info['name']} (PID: {proc.info['pid']}) finalizado com sucesso")
+                    return True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
+                print(f'{self.app_name}: Erro ao finalizar o processo: {e}')
 
-    def app_update(self):
-        response = requests.get('https://github.com/smedsarandi/remote_maintenance/raw/main/remote_maintenance.json', stream=True)
-        if response.status_code == 200:
-            response.raw.decode_content = True
-            remote_maintenance = json.load(response.raw)
-            if self.version < remote_maintenance[self.app_name]['version']:
-                print('Inicializando atualização')
-                self.app_stop()
-                self.app_download()
-                self.app_start()
+    # Método que ATUALIZA O APP
+    def app_update(self, remote_version, remote_url):
+        print(f'\n\nSOLICITADO UPDATE PARA {self.app_name}')
+        if self.version < remote_version:
+            print(f'{self.app_name}: Versão Local: {self.version}, Versão Server: {remote_version}')
+            print(f'{self.app_name}: Inicializando atualização')
+            self.url_download = remote_url
+            self.app_stop()
+            self.app_download()
+            self.app_start()
+            self.version = remote_version
         else:
-            return False
+            print(f'{self.app_name}: Não é necessário atualizar. Versão Local: {self.version}, Versão Server: {remote_version}')
 
-'''
-Esta função vai baixar o remote_maintenance.json e verificará se ESTE PC precisa executar algum APP.
-Se afirmativo, ele instanciará a classe App_manager com o nome do APP
-'''
+# Função para gerenciar cada app em uma thread separada
+def manage_app(app_manager, remote_version, remote_url):
+    if not app_manager.app_download():
+        return
+    app_manager.app_start()
+    while True:
+        app_manager.app_update(remote_version, remote_url)
+        time.sleep(time_loop)
+
+# Função que verifica se este PC precisa executar algum APP
 def get_new_app():
-    pass
-
-
-app1 = App_manager(app_name='teste1', version=1, url_download='https://github.com/smedsarandi/remote_maintenance/raw/main/apps/teste1/dist/teste1.zip', executable_name='teste1.exe')
-app1.app_download()
-app1.app_start()
-app1.app_update()
-
-
-
-'''
-
-#função usada para baixar novas configuraçõesm do json
-def downloader_json(url):
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        response.raw.decode_content = True
-        #shutil.copyfileobj(response.raw, file)
-        return json.load(response.raw)
-    else:
-        return False
-
-
-
-#loop que será chamado no intervalo de tempo definido no "time_loop"
-whil e True:
-    os.chdir(directory_executor)
-    remote_maintenance_new = downloader_json(url='https://github.com/smedsarandi/remote_maintenance/raw/gui/remote_maintenance.json')
+    global time_loop
     try:
-        #verifica se ja existe o remote_maintenance.json
-        if os.path.exists("remote_maintenance.json"):
-            #abrirá o arquivo atual para verificar a diferença de versão
-            with open('remote_maintenance.json', encoding='utf-8') as meu_json:
-                remote_maintenance_old = json.load(meu_json)
-            
-            if remote_maintenance_old['config']['version_json'] < remote_maintenance_new['config']['version_json']:
-                print(f'Versão desatualizada')
+        response = requests.get('https://github.com/smedsarandi/remote_maintenance/raw/main/remote_maintenance.json')
+        if response.status_code == 200:
+            data = response.json()
+            config = data.get('config', {})
+            time_loop = config.get('time_loop', 60)  # Tempo padrão de 60 segundos se não especificado
 
-                for chave, valor in remote_maintenance_new.items():
-                    if chave != 'config':
-                        for maquina in valor['maquinas']:
-                            if maquina == 'all' or maquina == hostname:
-                                ######## FALTA COMPARAR A VERSÃO ANTES DE MANDAR BAIXAR
-                                if valor['version'] > remote_maintenance_old[chave]['version']:
-                                    file_executor(app_name=chave, url=valor['url'], executable_name=valor['executable_name'])
+            app_managers = {}
 
-                                    with open('remote_maintenance.json', 'w') as file:
-                                        json.dump(remote_maintenance_old, file, indent=4)
-
-            else:
-                print('versão atualizada, não é necessário fazer mudanças')
+            while True:
+                for app_name, app_info in data.items():
+                    if app_name == 'config':
+                        continue
+                    maquinas = app_info.get('maquinas', [])
+                    if 'all' in maquinas or hostname in maquinas:
+                        if app_name not in app_managers:
+                            app_manager = App_manager(
+                                app_name=app_info['app_name'],
+                                version=0,  # Inicializa a versão como 0 para garantir que baixe a primeira vez
+                                url_download=app_info['url'],
+                                executable_name=app_info['executable_name']
+                            )
+                            app_managers[app_name] = app_manager
+                            threading.Thread(target=manage_app, args=(app_manager, app_info['version'], app_info['url'])).start()
+                time.sleep(time_loop)  # Espera pelo tempo definido antes de verificar novamente
         else:
-            #ainda não existe o arquivo
-            file_executor(app_name=chave, url=valor['url'], executable_name=valor['executable_name'])
-            #cria o arquivo
-            with open('remote_maintenance.json', 'w') as file:
-                json.dump(remote_maintenance_new, file, indent=4)
+            print('Falha ao baixar remote_maintenance.json')
+            time.sleep(60)  # Espera 60 segundos antes de tentar novamente em caso de falha
+    except requests.exceptions.RequestException as e:
+        print(f'Erro ao baixar o arquivo JSON: {e}')
+        time.sleep(60)  # Espera 60 segundos antes de tentar novamente em caso de erro de rede
+    except json.JSONDecodeError as e:
+        print(f'Erro ao decodificar o arquivo JSON: {e}')
+        time.sleep(60)  # Espera 60 segundos antes de tentar novamente em caso de erro de decodificação
+    except KeyError as e:
+        print(f'Erro na estrutura do arquivo JSON: {e}')
+        time.sleep(60)  # Espera 60 segundos antes de tentar novamente em caso de erro de estrutura
+    except Exception as e:
+        print(f'Erro inesperado: {e}')
+        time.sleep(60)  # Espera 60 segundos antes de tentar novamente em caso de erro inesperado
 
-
-    except:
-        print('não foi possivel baixar o json inicial') #Substituir este print por um log
-
-    time.sleep(remote_maintenance_new['time_loop'])
-
-'''
+# Exemplo de uso
+get_new_app()
