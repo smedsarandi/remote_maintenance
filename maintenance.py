ab2d7450d30eed72ad582e405ff3907ff7536dbe
fontesthiago <thiago.fontes.web@gmail.com>
2024-07-03 20:20:29 -0300

    removido variável add_quot que contava a quantidade de APPS e melhorado comentarios

--- maintenance.py
+++ maintenance.py
@@ -19,14 +19,13 @@ app_managers = [] #É uma lista que contêm todos os apps em execução no momen
 app_quot = 0 #Vai armazenar a quantidade de app que existe quando o app_managers for criado, pois se for criados novos apps para essa maquina, ele reconhecerá
 directory_executor = 'c:/apps/'
 
-# Verificação a existência do diretório de execução, caso contrário ele será criado
-if not os.path.exists(directory_executor):
-    os.makedirs(directory_executor)
-# definindo o diretório de execução
-os.chdir(directory_executor)
 
-# Configurando o logger
-logging.basicConfig(level=logging.INFO, 
+if not os.path.exists(directory_executor):# Verificação a existência do diretório de execução, caso contrário ele será criado
+    os.makedirs(directory_executor)
+os.chdir(directory_executor) # definindo o diretório de execução
+
+
+logging.basicConfig(level=logging.INFO, # Configurando o logger
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     handlers=[
                         logging.FileHandler("remote_maintenance.log"),
@@ -34,20 +33,17 @@ logging.basicConfig(level=logging.INFO,
                     ])
 logger = logging.getLogger()
 
-''' 
-Classe que gerenciará os apps já baixados e possivelmente em execução que foram filtrados pela função "initialize()"
-Cada APP será uma instancia dessa classe e a partir dela será possivel baixar, iniciar, parar e atualizar o APP individualmente
-'''
+
+#Classe que gerenciará os apps já baixados e possivelmente em execução que foram filtrados pela função "initialize()"
+#Cada APP será uma instancia dessa classe e a partir dela será possivel baixar, iniciar, parar e atualizar o APP individualmente
 class App_manager:
-    #as propriedades que serão instanciadas para cada APP
-    def __init__(self, app_name, version, url_download, executable_name):
+    def __init__(self, app_name, version, url_download, executable_name): #as propriedades que serão instanciadas para cada APP
         self.app_name = app_name
         self.version = version
         self.url_download = url_download
         self.executable_name = executable_name
 
-    # Método que apenas baixa e extrai o arquivo .zip da url no 'directory_executor' definido nas variaveis globais
-    def app_download(self):
+    def app_download(self): # Método que apenas baixa e extrai o arquivo .zip da url no 'directory_executor' definido nas variaveis globais
         logger.info(f'{self.app_name}: em download')
         try:
             response = requests.get(self.url_download, stream=True)
@@ -78,8 +74,7 @@ class App_manager:
             logger.error(f'{self.app_name}: Erro inesperado: {e}')
             return False
 
-    # Método que INICIA O APP
-    def app_start(self):
+    def app_start(self):# Método que INICIA O APP
         logger.info(f'{self.app_name}: em start')
         try:
             executable_path = os.path.join(directory_executor, self.executable_name)
@@ -91,8 +86,7 @@ class App_manager:
         except Exception as e:
             logger.error(f'{self.app_name}: Erro ao iniciar o app: {e}')
 
-    # Método que PARA O APP
-    def app_stop(self):
+    def app_stop(self):# Método que PARA O APP
         logger.info(f'{self.app_name}: em stop')
         for proc in psutil.process_iter(['pid', 'name']):
             try:
@@ -105,8 +99,7 @@ class App_manager:
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
                 logger.error(f'{self.app_name}: Erro ao finalizar o processo: {e}')
 
-    # Método que ATUALIZA O APP
-    def app_update(self, remote_version):
+    def app_update(self, remote_version):# Método que ATUALIZA O APP
         logger.info(f'{self.app_name}: em update')
         if self.version < remote_version:
             logger.info(f'{self.app_name}: Versão Local: {self.version}, Versão Server: {remote_version}.\nSerá feito update!!')
@@ -118,13 +111,11 @@ class App_manager:
         else:
             logger.info(f'{self.app_name}: Desnecessário atualizar. Versão Local: {self.version}, Versão Server: {remote_version}')
 
-'''
-Está função será colocada em outra função de loop;
-Ela é responsavel por verificar no json remoto se será preciso iniciar algum app;
-Se APPS ja estão inicializados ela vetificará a versão atual para possiveis update.
-'''
+
+#Está função será colocada em outra função de loop;
+#Ela é responsavel por verificar no json remoto se será preciso iniciar algum app;
+#Se APPS ja estão inicializados ela vetificará a versão atual para possiveis update.
 def initialize():
-    print(f'ao chamar initialize o app_managers é: {len(app_managers)}')
     global app_quot
     app_quot_remote = 0
     headers = {
@@ -132,19 +123,20 @@ def initialize():
         'Pragma': 'no-cache'
     }
     response = requests.get('https://github.com/smedsarandi/remote_maintenance/raw/main/remote_maintenance.json', headers=headers)
-    if response.status_code == 200: 
-        json_remote = response.json()  # Decodifica o conteúdo JSON da resposta
-        #esse "app_managers" é uma lista que contêm todos os apps em execução no momento
-        for key, value in json_remote.items():
-            if 'maquinas' in value:
-                if hostname in value['maquinas'] or 'all' in value['maquinas']:
-                    app_instanciado = False
-                    for app in app_managers:
-                        logger.info(f'{app.app_name} já está instanciado')
-                        if app.app_name == key:
+    if response.status_code == 200: #só vai executar se o statuscode for 200 (ok)
+        json_remote = response.json()  #Decodifica o conteúdo da resposta e cria essa variavel com o json dentro
+        
+        for key, value in json_remote.items(): #vai interar sobre cada item do json remoto
+            if 'maquinas' in value: #se dentro dos valores existir o 'atributo' 'maquinas'
+                if hostname in value['maquinas'] or 'all' in value['maquinas']: # vai verificar se nessa lista de maquina se encontra este pc
+                    app_instanciado = False # por padrao ele vai considerar que nao existe a instancia do app remoto, a nao ser que a proxima linha prove isso
+                    for app in app_managers:#esse "app_managers" é uma lista que contêm todos os apps em execução no momento
+                        if app.app_name == key:                            
                             app_instanciado = True
-                            app.app_update(remote_version=value['version'])     
-                    if app_instanciado == False:
+                            logger.info(f'{app.app_name}: já está instanciado') #identificou que o APP ja esta instanciado na maquina, entao ele vai buscar por atts
+                            app.app_update(remote_version=value['version'])   
+                    
+                    if app_instanciado == False: #agora se o APPS nao estiver instanciado, entao esta parte vai instancialo
                         logger.info(f'{key}: instanciando')
                         app_manager = App_manager(app_name=key, version=value['version'], url_download=value["url"], executable_name=value["executable_name"])
                         app_manager.app_download()
@@ -154,17 +146,15 @@ def initialize():
     else:
         logger.error('Não foi possivel baixar json com novas informações')
 
-        
-def initialize_loop():
 
+def initialize_loop():
     while True:
         global app_managers
-        #print(f'##########o tamano do app_managers é:{len(app_managers)}')
         logger.info('\n\n')
+        logger.info(f'##########o tamano do app_managers atualmente é:{len(app_managers)}')
         logger.warning("INICIALIZANDO LOOP")
         initialize()
-        time.sleep(5)
-        
+        time.sleep(10)
 
 
 initialize_loop()
