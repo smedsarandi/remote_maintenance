314de4e36f1a83a627b0b762a6fb472dd771c337
fontesthiago <thiago.fontes.web@gmail.com>
2024-07-03 22:22:10 -0300

    corrigido o install.py que estava baixando arquivo corrompido

--- install.py
+++ install.py
@@ -7,7 +7,8 @@ import time
 import requests
 import shutil
 import subprocess
-# Variaveis globais
+
+# Variáveis globais
 url_maintenance = 'https://github.com/smedsarandi/remote_maintenance/raw/main/dist/maintenance.zip'
 arquivo_exe = 'maintenance.exe'
 arquivo_zip_destino = 'c:/Windows/Temp/maintenance.zip'
@@ -15,30 +16,29 @@ arquivo_exe_destino = 'c:/Windows/Temp/maintenance.exe'
 
 logging.basicConfig(level=logging.INFO, filename="install.log", format="%(asctime)s - %(levelname)s - %(message)s")
 
-
 def download_maintenance_exe(url):
-    response = requests.get(url)
+    response = requests.get(url, stream=True)
     if response.status_code == 200:
         with open(arquivo_zip_destino, 'wb') as file:
-            response.raw.decode_content = True
-            shutil.copyfileobj(response.raw, file)
-            logging.info('arquivo baixado')
-        with zipfile.ZipFile(arquivo_zip_destino, 'r') as Zip:
-            logging.info(f'extraindo arquivo zip')
-            Zip.extractall()
-            time.sleep(5)
-            os.remove(arquivo_zip_destino)
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        logging.info('Arquivo baixado')
+        
+        with zipfile.ZipFile(arquivo_zip_destino, 'r') as zip_ref:
+            logging.info('Extraindo arquivo zip')
+            zip_ref.extractall('c:/Windows/Temp')
+        
+        time.sleep(5)
+        os.remove(arquivo_zip_destino)
+    else:
+        logging.error(f"Erro ao baixar o arquivo: Status code {response.status_code}")
 
 download_maintenance_exe(url=url_maintenance)
 
 try:
-    #O os.popen abre um 'terminal' e executa o comando... nesse caso ele executara o 'schtasks' que no windows é responsavel por criar tarefas
-    subprocess.Popen('schtasks /create /sc ONLOGON /ru System /tr c:\Windows\Temp\maintenance.exe /tn Microsoft\Windows\Maintenance\maintenance', shell=True)
-    #os.popen(rf'schtasks /create /sc ONLOGON /ru System /tr c:\Windows\Temp\maintenance.exe /tn Microsoft\Windows\Maintenance\maintenance')
-    logging.info('maintenance task criada')
-except:
-    logging.error('maintenance task NÃO foi criada')
+    subprocess.Popen('schtasks /create /sc ONLOGON /ru System /tr c:\\Windows\\Temp\\maintenance.exe /tn Microsoft\\Windows\\Maintenance\\maintenance', shell=True)
+    logging.info('Tarefa maintenance criada')
+except Exception as e:
+    logging.error(f'Tarefa maintenance NÃO foi criada: {e}')
 
 time.sleep(2)
-
-#diretorio_atual = os.getcwd()
